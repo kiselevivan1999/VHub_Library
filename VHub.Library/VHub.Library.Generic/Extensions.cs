13a075c7e07b5a0f09c02f4c09531c8f41dae346
Kiselev Ivan <in.kiselev@cit42.ru>
2025-12-05 15:31:10 +0700

    Создал проект

---
+++ VHub.Library/VHub.Library.Generic/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using VHub.Library.Generic.Repository;
+
+namespace VHub.Library.Generic;
+
+public static class Extensions
+{
+    /// <summary>
+    /// Регистрация GenericRepository
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static IServiceCollection AddGenericRepository(this IServiceCollection services)
+    {
+        if (services == null)
+            throw new ArgumentNullException(nameof(services));
+
+        //services.AddScoped<IUnitOfWork, EfUnitOfWork>();
+
+        // Регистрируем универсальный репозиторий
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+        return services;
+    }
+}
